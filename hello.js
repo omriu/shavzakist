2f6ad8297f229268b7e18bca34fd5b768abdc40d
עומרי אוליאל <ulielomri@gmail.com>
2022-06-01 22:25:23 +0300

    end task

--- hello.js
+++ hello.js
@@ -1,10 +1,18 @@
 let omriuliel = () => {
-	document.getElementById('omri').innerHTML = 'OmriUleil';
+
+	//	document.getElementById('omri').innerHTML = 'OmriUleil';
 
 }
 
+function togglePopup() {
+       var popup = document.getElementById("myPopup");
+       popup.classList.toggle("hidden");
+       }
 
-
+document.querySelector('.popuptext')
+	.addEventListener('click', (e) => {
+	e.stopPropagation();
+	})
 	// Change text from Omri to OmriUleil
 	//	document.getElementById("h1").innerHTML = "OmriUleil";
 	//	document.createTextNode("OmriUleil");
